8ed5d46b575d908a389cc29ea26454580f22d6c3
Aspen Smith <root@gws.fyi>
2025-11-10 23:24:10 -0500

    Offload stepper driver completely onto core1 (second core)

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -2,26 +2,28 @@
 #![no_main]
 #![feature(impl_trait_in_assoc_type)]
 
-use core::net::Ipv4Addr;
+use core::{net::Ipv4Addr, ptr::addr_of_mut};
 
 use crate::a4988::Direction;
 use cyw43::{Control, JoinOptions};
 use cyw43_pio::{PioSpi, DEFAULT_CLOCK_DIVIDER};
 use defmt::*;
-use embassy_executor::Spawner;
+use embassy_executor::{Executor, Spawner};
 use embassy_net::{tcp::TcpSocket, Ipv4Cidr, StackResources};
 use embassy_rp::{
     bind_interrupts,
     clocks::RoscRng,
     gpio::{Level, Output},
+    multicore::Stack,
     peripherals::{DMA_CH0, PIO0},
     pio::{InterruptHandler, Pio},
 };
-use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
 use embassy_time::{Duration, Timer};
 use embedded_io_async::Write;
 use heapless::Vec;
 use picoserve::make_static;
+use static_cell::StaticCell;
 
 use {defmt_rtt as _, panic_probe as _};
 
@@ -53,7 +55,7 @@ async fn net_task(mut runner: embassy_net::Runner<'static, cyw43::NetDriver<'sta
 async fn server_task(
     stack: embassy_net::Stack<'static>,
     mut control: Control<'static>,
-    command_signal: &'static Signal<NoopRawMutex, gcode::Command<AXES>>,
+    command_signal: &'static Signal<CriticalSectionRawMutex, gcode::Command<AXES>>,
 ) -> ! {
     let mut rx_buffer = [0; 1024];
     let mut tx_buffer = [0; 1024];
@@ -113,7 +115,7 @@ async fn server_task(
                 {
                     blink_once(&mut control).await;
                     command_signal.signal(command);
-                    if let Err(e) = socket.write_all(b"gotcha!").await {
+                    if let Err(e) = socket.write_all(b"gotcha!\n").await {
                         warn!("write error: {}", e);
                         continue 'accept;
                     }
@@ -121,7 +123,7 @@ async fn server_task(
                 _ => {
                     blink_once(&mut control).await;
                     blink_once(&mut control).await;
-                    if let Err(e) = socket.write_all(b"huh??").await {
+                    if let Err(e) = socket.write_all(b"huh??\n").await {
                         warn!("write error: {}", e);
                         continue 'accept;
                     }
@@ -135,7 +137,7 @@ async fn server_task(
 #[embassy_executor::task]
 async fn stepper_driver_task(
     mut driver: a4988::Driver<AXES>,
-    command_signal: &'static Signal<NoopRawMutex, gcode::Command<AXES>>,
+    command_signal: &'static Signal<CriticalSectionRawMutex, gcode::Command<AXES>>,
 ) -> ! {
     loop {
         let command = command_signal.wait().await;
@@ -166,25 +168,15 @@ async fn blink_once(control: &mut Control<'_>) {
     control.gpio_set(0, false).await;
 }
 
-#[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    let p = embassy_rp::init(Default::default());
+#[embassy_executor::task]
+async fn core0(
+    pwr: Output<'static>,
+    spi: PioSpi<'static, PIO0, 0, DMA_CH0>,
+    spawner: Spawner,
+    command_signal: &'static Signal<CriticalSectionRawMutex, gcode::Command<AXES>>,
+) {
     let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
     let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
-
-    let pwr = Output::new(p.PIN_23, Level::Low);
-    let cs = Output::new(p.PIN_25, Level::High);
-    let mut pio = Pio::new(p.PIO0, Irqs);
-    let spi = PioSpi::new(
-        &mut pio.common,
-        pio.sm0,
-        DEFAULT_CLOCK_DIVIDER,
-        pio.irq0,
-        cs,
-        p.PIN_24,
-        p.PIN_29,
-        p.DMA_CH0,
-    );
 
     let state = make_static!(cyw43::State, cyw43::State::new());
     let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
@@ -217,10 +209,31 @@ async fn main(spawner: Spawner) {
         info!("join failed with status={}", err.status);
     }
 
-    let command_signal = make_static!(Signal<NoopRawMutex, gcode::Command<AXES>>, Signal::new());
-
     // let control = make_static!(Mutex<NoopRawMutex, Control>, Mutex::new(control));
     spawner.must_spawn(server_task(stack, control, command_signal));
+}
+
+static mut CORE1_STACK: Stack<4096> = Stack::new();
+static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
+static EXECUTOR1: StaticCell<Executor> = StaticCell::new();
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    let p = embassy_rp::init(Default::default());
+
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        DEFAULT_CLOCK_DIVIDER,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
 
     let driver = a4988::Driver::builder()
         .direction_pin(p.PIN_15)
@@ -231,5 +244,20 @@ async fn main(spawner: Spawner) {
             Output::new(p.PIN_19, Level::Low),
         ])
         .build();
-    spawner.must_spawn(stepper_driver_task(driver, command_signal));
+
+    static COMMAND_SIGNAL: StaticCell<Signal<CriticalSectionRawMutex, gcode::Command<AXES>>> =
+        StaticCell::new();
+    let command_signal: &'static _ = COMMAND_SIGNAL.init(Signal::new());
+
+    embassy_rp::multicore::spawn_core1(
+        p.CORE1,
+        unsafe { &mut *addr_of_mut!(CORE1_STACK) },
+        move || {
+            let executor1 = EXECUTOR1.init(Executor::new());
+            executor1.run(|spawner| spawner.must_spawn(stepper_driver_task(driver, command_signal)))
+        },
+    );
+
+    let executor0 = EXECUTOR0.init(Executor::new());
+    executor0.run(|spawner| spawner.must_spawn(core0(pwr, spi, spawner, command_signal)))
 }
