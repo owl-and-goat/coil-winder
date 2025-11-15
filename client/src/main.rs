2b841ee8b0c74904ae7a1cdddc9b56fbba3d90ae
Aspen Smith <root@gws.fyi>
2025-11-14 21:36:39 -0500

    Factor out client handling, add a run command that takes a file
    This is what we'll actually use in the real-world to run gcode programs

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,7 @@
+use std::fs::File;
 use std::io::{self, BufRead, BufReader, ErrorKind, Write};
-use std::net::{SocketAddr, TcpStream};
+use std::net::{SocketAddr, TcpStream, ToSocketAddrs};
+use std::path::PathBuf;
 
 use clap::Parser;
 use eyre::{bail, Result};
@@ -8,11 +10,29 @@ use rustyline::{error::ReadlineError, DefaultEditor};
 
 #[derive(clap::Subcommand, Debug)]
 enum Command {
+    /// Interactive REPL for sending gcode commands
     Repl,
+
+    /// Send a single Gcode command from the command line
     Oneshot {
         /// Command to send
         #[clap(long, short)]
         command: String,
+    },
+
+    /// Run a gcode program
+    Run {
+        /// Path to gcode program to run
+        #[clap(long, short)]
+        path: PathBuf,
+
+        /// Skip verification that the program parses before running
+        #[clap(long)]
+        no_verify: bool,
+
+        /// Print responses from the chip after each command
+        #[clap(long)]
+        print_responses: bool,
     },
 }
 
@@ -26,33 +46,121 @@ struct Args {
     command: Command,
 }
 
+fn read_commands_from_file(path: PathBuf, verify: bool) -> Result<Vec<String>> {
+    let reader = BufReader::new(File::open(&path)?);
+    let mut res = Vec::new();
+    if !verify {
+        for line in reader.lines() {
+            res.push(line?);
+        }
+    } else {
+        for (i, line) in reader.lines().enumerate() {
+            let line = line?;
+            let line = line.trim();
+            if line.starts_with('(') {
+                if !line.ends_with(')') {
+                    bail!("{}:{i}: Mismatched parenthesis in comment", path.display());
+                }
+                continue;
+            }
+            let line = match line.rsplit_once(';') {
+                Some((line, _comment)) => line,
+                None => line,
+            };
+            let mut line = line.to_owned();
+            if !line.ends_with('\n') {
+                line.push('\n');
+            }
+            match gcode::parse_single_command(['X', 'Y', 'Z', 'F'], line.as_bytes()) {
+                Ok(_) => {}
+                Err(_) => bail!("{}:{i}: Invalid gcode command", path.display()),
+            };
+            res.push(line);
+        }
+    }
+
+    Ok(res)
+}
+
+pub struct Client {
+    stream: TcpStream,
+    reader: BufReader<TcpStream>,
+}
+
+impl Client {
+    pub fn connect(addr: impl ToSocketAddrs) -> io::Result<Self> {
+        let stream = TcpStream::connect(addr)?;
+        let reader = BufReader::new(stream.try_clone()?);
+        Ok(Self { stream, reader })
+    }
+
+    fn reconnect(&mut self) -> io::Result<()> {
+        let addr = self.stream.peer_addr()?;
+        self.stream = TcpStream::connect(addr)?;
+        self.reader = BufReader::new(self.stream.try_clone()?);
+        Ok(())
+    }
+
+    pub fn send(&mut self, mut command: String) -> io::Result<String> {
+        if !command.ends_with('\n') {
+            command.push('\n');
+        }
+
+        loop {
+            match self.stream.write_all(command.as_bytes()) {
+                Ok(()) => break,
+                Err(err) => match err.kind() {
+                    ErrorKind::ConnectionReset | ErrorKind::BrokenPipe => {
+                        self.reconnect()?;
+                        continue;
+                    }
+                    _ => return Err(err.into()),
+                },
+            }
+        }
+        self.stream.flush()?;
+        let mut resp = String::new();
+        if self.reader.read_line(&mut resp)? == 0 {
+            self.reconnect()?;
+        }
+        Ok(resp)
+    }
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
     let mut rl = DefaultEditor::new()?;
     let mut history = rustyline::history::MemHistory::new();
-
-    let mut stream = TcpStream::connect(args.addr)?;
-    let mut reader = BufReader::new(stream.try_clone()?);
+    let mut client = Client::connect(args.addr)?;
 
     match args.command {
-        Command::Oneshot { mut command } => {
-            command.push('\n');
-            stream.write_all(command.as_bytes())?;
-            stream.flush()?;
-            let mut resp = String::new();
-            if reader.read_line(&mut resp)? == 0 {
+        Command::Oneshot { command } => {
+            let resp = client.send(command)?;
+            if resp.len() == 0 {
                 bail!("Got EOF from server");
             }
             println!("{resp}");
             Ok(())
         }
-        Command::Repl => {
-            let reconnect = || -> io::Result<_> {
-                let stream = TcpStream::connect(args.addr)?;
-                let reader = BufReader::new(stream.try_clone()?);
-                Ok((stream, reader))
-            };
+        Command::Run {
+            path,
+            no_verify,
+            print_responses,
+        } => {
+            let commands = read_commands_from_file(path, !no_verify)?;
+            let n = commands.len();
+            for command in commands {
+                let resp = client.send(command)?;
+                if print_responses {
+                    println!("{resp}");
+                }
+            }
 
+            println!("Successfully ran {n} commands");
+
+            Ok(())
+        }
+        Command::Repl => {
             loop {
                 match rl.readline(">> ") {
                     Err(ReadlineError::Eof) => break,
@@ -61,28 +169,9 @@ fn main() -> Result<()> {
                         eprintln!("{err}");
                         break;
                     }
-                    Ok(mut line) => {
-                        history.add(&line)?;
-                        line.push('\n');
-                        loop {
-                            match stream.write_all(line.as_bytes()) {
-                                Ok(()) => break,
-                                Err(err) => match err.kind() {
-                                    ErrorKind::ConnectionReset | ErrorKind::BrokenPipe => {
-                                        (stream, reader) = reconnect()?;
-                                        continue;
-                                    }
-                                    _ => return Err(err.into()),
-                                },
-                            }
-                        }
-                        stream.flush()?;
-                        let mut resp = String::new();
-                        if reader.read_line(&mut resp)? == 0 {
-                            (stream, reader) = reconnect()?;
-                        } else {
-                            println!("{resp}");
-                        }
+                    Ok(command) => {
+                        history.add(&command)?;
+                        client.send(command)?;
                     }
                 }
             }
