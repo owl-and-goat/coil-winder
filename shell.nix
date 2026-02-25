79cad88150650694f90a28805d7bd8bf186f967e
Aspen Smith <root@gws.fyi>
2026-02-24 20:50:49 -0500

    Pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,8 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (builtins.fetchTarball {
+  url =
+    "https://github.com/nixos/nixpkgs/archive/50ab793786d9de88ee30ec4e4c24fb4236fc2674.tar.gz";
+  sha256 = "1s2gr5rcyqvpr58vxdcb095mdhblij9bfzaximrva2243aal3dgx";
+}) { } }:
 
 with pkgs;
 
